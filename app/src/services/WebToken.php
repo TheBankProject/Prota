8e0181e90a01c83206fc087efed264d51e936596
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-10-27 00:02:27 -0300

    adding jwt basics

---
+++ app/src/services/WebToken.php
@@ -0,0 +1,35 @@
+<?php
+namespace Minuz\BaseApi\services;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+session_start();
+class WebToken
+{
+    public static function Login(array|bool $auth)
+    {
+        $token = JWT::encode($auth, $_ENV['JWT_KEY'], 'HS256');
+        return;
+    }
+
+
+
+    public static function Signup(array|bool $auth)
+    {
+
+    }
+
+
+
+    public static function SessionLogin()
+    {
+        try {
+            $info = JWT::decode($token, new Key($_ENV['JWT_KEY'], 'HS256'));
+        } catch (\UnexpectedValueException $e) {
+            return;
+        }
+
+        return;
+    }
+}
